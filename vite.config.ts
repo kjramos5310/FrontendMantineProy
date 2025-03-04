892b86b6b7ae2209e9d63af3fb9884d197708761
kjramos5310 <105337663+kjramos5310@users.noreply.github.com>
2025-03-04 18:06:00 -0500

    Funcional Mantine

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  css: {
+    modules: {
+      localsConvention: 'camelCase', // Opcional, para evitar problemas con nombres de clases
+      scopeBehaviour: 'local',
+    },
+  },
+});
